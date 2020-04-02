23fb1123e91f59c4cec7a3e63b49ebddca9f7c17
Zhiyuan WANG <wwzzyy9982@gmail.com>
2020-04-02 10:41:58 +0800

    Initial commit

--- view.sh
+++ view.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-hugo --i18n-warnings server
+cd /Users/wang/My_Website/
+rm -r public
+git submodule add -f -b master https://github.com/zzhiyuann/zzhiyuann.github.io.git public
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+
+hugo
+cd public
+cp -r /Users/wang/blog/public /Users/wang/My_Website/public/blog
+git add .
+git commit -m "Build website"
+git pull
+git push -f origin master
+cd ..
